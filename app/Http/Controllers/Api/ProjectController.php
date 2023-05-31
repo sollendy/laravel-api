0e8ae52362c28854af18ca19d5032872c7dad309
sollendy <edlannino@tutanota.com>
2023-05-31 20:07:51 +0200

    in corso request per bonus in front end

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,20 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         // restituisce tutti i post nel db
         // $projects = Post::all();
-        $projects = Project::with("type", "technologies")->orderBy("projects.created_at", "desc")->paginate(6);
+        $projects = Project::with("type", "technologies")
+        ->orderBy("projects.created_at", "desc")
+        ->paginate(6);
             // ->orderBy('projects.created_at', 'desc')
             // ->paginate(6);
 
+        $types = Type::all();
+
+        if($request->has("type_id")) {
+
+            $requestData = $request->all();
+            dd($requestData);
+
+        }
+
         return response()->json([
             'success' => true,
-            'results' => $projects
+            'results' => $projects,
+            'allTypes' => $types,
         ]);
     }
 
